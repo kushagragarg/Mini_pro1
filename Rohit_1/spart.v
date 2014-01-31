0bfdd2593ee1aba422ae9ec214b016b582ea87b1
rshukla3 <rshukla3@wisc.edu>
2014-01-31 16:23:08 -0600

    Modified the spart module as per the port names declared in reciever module.

--- Rohit_1/spart.v
+++ Rohit_1/spart.v
@@ -3,11 +3,11 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:           30 Jan 2014
-// Design Name:         SPART
-// Module Name:            spart
+// Create Date:   	30 Jan 2014
+// Design Name: 	SPART
+// Module Name:    	spart
 // Project Name: 
-// Target Devices:         Xilinx Virtex 5
+// Target Devices: 	Xilinx Virtex 5
 // Tool versions: 
 // Description: 
 //
@@ -32,60 +32,61 @@ module spart(
     );
 
     wire [7:0] rx_databus;
-        wire brg_en, brg_full;
-        reg clr_rda;
-        reg rx_tri_en, status_tri_en, brg_tri_en;
+	wire brg_en, brg_full;
+	reg clr_rda;
+	reg rx_tri_en, status_tri_en, brg_tri_en;
 
-        // Instantiate sub-modules
-        brg DUT_brg(.databus(databus),
-                                .clk(clk),
-                                .rst(rst),
-                                .brg_en(brg_en),
-                                .brg_full(brg_full),
-                                .ioaddr(ioaddr)
-                                );
+	// Instantiate sub-modules
+	brg DUT_brg(.databus(databus),
+				.clk(clk),
+				.rst(rst),
+				.brg_en(brg_en),
+				.brg_full(brg_full),
+				.ioaddr(ioaddr)
+				);
 /*
-        transmit DUT_tx(.databus(databus),
-                                        .clk( clk),
-                                        .rst( rst),
-                                        .tbr(tbr),
-                                        .brg_full(brg_full),
-                                        .txd(txd),
-                                        .ioaddr(ioaddr),
-                                        .iorw(iorw),
-                                        .iocs(iocs)
-                                        );
+	transmit DUT_tx(.databus(databus),
+					.clk( clk),
+					.rst( rst),
+					.tbr(tbr),
+					.brg_full(brg_full),
+					.txd(txd),
+					.ioaddr(ioaddr),
+					.iorw(iorw),
+					.iocs(iocs)
+					);
 */
-        receiver DUT_rx(.DATABUS(rx_databus), 
-                                        .clk(clk), 
-                                        .rst(rst),
-                                        .RDA(rda),
-                                        .RX(rxd),
-                                        .brg_en(brg_en),
-                                        .clr_rda(clr_rda)
-                                        );
 
-        // Bus Interface
-        // Enable tri-states for Receiver and Status to drive the bus when required.
-        always @(*) begin
-        rx_tri_en = 1'b0;
-        status_tri_en = 1'b0;
-        clr_rda = 1'b0;
-                if (iocs) begin
-                        if (ioaddr == 2'b00 && iorw == 1'b1) begin
-                                rx_tri_en = 1'b1;
-                                clr_rda = 1'b1;
-                        end
-                        if (ioaddr == 2'b01 && iorw == 1'b1) 
-                                status_tri_en = 1'b1;
-                end
-                else begin
-                        rx_tri_en = 1'b0;
-                        status_tri_en = 1'b0;
-                end
-        end
+	receiver DUT_rx(.DATABUS(rx_databus), 
+					.clk(clk), 
+					.rst(rst),
+					.RDA(rda),
+					.RX(rxd),
+					.brg_en(brg_en),
+					.clr_rda(clr_rda)
+					);
 
-        assign databus = rx_tri_en ? rx_databus : (status_tri_en ? {6'h00,tbr,rda}: 8'hzz); 
+	// Bus Interface
+	// Enable tri-states for Receiver and Status to drive the bus when required.
+	always @(*) begin
+	rx_tri_en = 1'b0;
+	status_tri_en = 1'b0;
+	clr_rda = 1'b0;
+		if (iocs) begin
+			if (ioaddr == 2'b00 && iorw == 1'b1) begin
+				rx_tri_en = 1'b1;
+				clr_rda = 1'b1;
+			end
+			if (ioaddr == 2'b01 && iorw == 1'b1) 
+				status_tri_en = 1'b1;
+		end
+		else begin
+			rx_tri_en = 1'b0;
+			status_tri_en = 1'b0;
+		end
+	end
 
-        
+	assign databus = rx_tri_en ? rx_databus : (status_tri_en ? {6'h00,tbr,rda}: 8'hzz); 
+
+	
 endmodule
