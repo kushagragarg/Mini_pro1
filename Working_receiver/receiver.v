5e6cca4de18844258c905d19a2680e7704229896
kushagragarg <kushagragarg.bits@gmail.com>
2014-02-01 17:20:49 -0600

    Updates

--- Working_receiver/receiver.v
+++ Working_receiver/receiver.v
@@ -25,7 +25,8 @@ module receiver(
 	input 		 brg_en,
 	input 		 clk,
 	input 		 rst,
-	input 		 clr_rda		 
+	input 		 clr_rda,
+	input 		 brg_full
     );
 
 	// Counter increments from 0 to 9. After its value becomes 9, this means
@@ -38,17 +39,17 @@ module receiver(
 	reg [1:0] next_state;
 	// Store the one bit input data in the register first.
 	reg received_input_bit;
-
-	//Counter that counts the number of samples.
+	reg previous_input_bit;
+	// Count the number of samples taken till now.
 	reg [3:0] sampleCounter;
-	reg [3:0] bitCounter;
-	
 	 
-	// Store the received bits in the ReceivedData register.
+	// Sore the received bits in the ReceivedData register.
 	// When all of the input serial data has been received
 	// send it to the processor through DATABUS.
 	reg [7:0] ReceivedData;
-    
+   reg inc_sampleCounter; 
+	 
+	  
 	assign DATABUS = ReceivedData;
 
    ////////////
@@ -69,76 +70,64 @@ module receiver(
 	begin
 		if(rst)
 		begin
+			sampleCounter <= 8'h0;
+		end	
+		else
+		begin
+			if(brg_full)
+			begin
+				sampleCounter <= 0;
+			end
+			else if(brg_en && received_input_bit)
+			begin
+				sampleCounter <= sampleCounter + 1;
+			end				
+		end
+	end
+	
+	
+	always @(posedge clk)
+	begin
+		if(rst)
+		begin
 			ReceivedData <= 8'h0;
 			received_input_bit <= 1'b0;
 			state <= NOT_RECEIVING_DATA;
-			counter <= 4'h0; // Counter for counting 9 bits including start bit
+			counter <= 4'h0;
 			next_state <= NOT_RECEIVING_DATA;
 			RDA <= 1'b0;
-			sampleCounter <= 4'h0;
-			bitCounter <= 4'h0;
 		end
-		
 		else
-		
 		begin
-			received_input_bit <= RX; // Synchronizing received bit wrt to clk
+			received_input_bit <= RX;
 			state <= next_state;
-			if( ( (received_input_bit == START_BIT) | (sampleCounter > 0) ) && (state == NOT_RECEIVING_DATA) && brg_en)  
+			if( (sampleCounter[3] == START_BIT) && (state == NOT_RECEIVING_DATA) && brg_full)
 			begin
-				// next_state <= RECEIVE_DATA;
-				//counter <= counter+1;
-			    sampleCounter <= sampleCounter + 1;
-			end
-			
-			else if( (state == NOT_RECEIVING_DATA) && brg_en && (sampleCounter == 15) )
-			begin
-				if(bitCounter[3] == 0)  // It's start bit
-    			next_state <= RECEIVE_DATA;
-				
+				next_state <= RECEIVE_DATA;
+				ReceivedData <= 8'h0;
 				counter <= counter+1;
-				sampleCounter <= 4'h0;
-				bitCounter <= 4'h0;
 			end
-			
-			
-			else if( (state == RECEIVE_DATA) && (counter != COMPLETED_RECEIVING_DATA) && brg_en)
-			begin
-			if(received_input_bit)   // If its a 1, increment bitCounter
-				begin
-				bitCounter <= bitCounter + 1;
-				end
-			sampleCounter <= sampleCounter + 1;
-			end
-			
-			else if( (state == RECEIVE_DATA) && (counter != COMPLETED_RECEIVING_DATA) && brg_en && (sampleCounter == 15))
-			begin
-			if(bitCounter[3] == 0)  // It's 0 bit bit
-    			ReceivedData <= {ReceivedData[6:0], 1'b0};
 			else
-                ReceivedData <= {ReceivedData[6:0], 1'b1};
-				
-				counter <= counter+1;
-				sampleCounter <= 4'h0;
-				bitCounter <= 4'h0;
-			end
-			
-			
+			begin
+				if( (state == RECEIVE_DATA) && (counter != COMPLETED_RECEIVING_DATA) && brg_full )
+				begin
+					ReceivedData <= {sampleCounter[3], ReceivedData[7:1]};
+					counter <= counter+1;
+				end
 				else if((state == RECEIVE_DATA) && (counter == COMPLETED_RECEIVING_DATA))
 				begin
 					next_state <= SET_RDA;
 					counter <= 4'h0;
      				RDA <= 1'b1;
 				end
-				
 				else if((state == SET_RDA) && (RDA == 1'b1) && (clr_rda == 1'b1))
 				begin
 					next_state <= NOT_RECEIVING_DATA;
 					RDA <= 1'b0;
 			//		DATABUS <= ReceivedData; // Needn't assign receiver here .... since read is asynchronous
-					ReceivedData <= 8'b0;
+					
 				end     
-			
+			end
 			
 		end
 	end
---
+++ Working_receiver/tb_top.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ps
+
+module tb_top;
+reg clk, rst, RX;
+reg [1:0] br_cfg;
+wire txd;
+
+top_level U0 (
+.clk  (clk),
+.rst  (rst),
+.txd (txd),
+.rxd (RX),
+.br_cfg (br_cfg)
+);
+
+
+
+initial
+begin
+clk = 0;
+rst = 1;
+RX = 1;
+br_cfg = 2'b01;
+
+#40 rst = 0;
+
+assign RX = txd;
+
+end
+
+always 
+#5 clk = ~clk;
+
+endmodule
